d557ff522409573056f11ee48ad96832043093f0
praveen1817 <praveen42165@gmail.com>
2026-02-03 17:03:13 +0530

    The Navbar for the Apllication is Designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar/>
     </>
   )
 }
